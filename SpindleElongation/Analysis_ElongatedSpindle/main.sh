23435de66aef8a807684fe1a9dec0a8c1b9c0f71
Takahiro Yamada <yamada@fun.bio.keio.ac.jp>
2022-07-05 14:13:18 +0900

    main.sh was updated in spindle elongation.

---
+++ SpindleElongation/Analysis_ElongatedSpindle/main.sh
@@ -0,0 +1,26 @@
+cd ./src/SimulationCode/celegans/
+./because_of_graph.sh -m 0 -l 0
+mv result.csv ../../../Result/Simulation/Cel_MTFixed.csv
+./because_of_graph.sh -m 1 -l 0
+mv result.csv ../../../Result/Simulation/Cel_MTVariable.csv
+
+cd ../sea-urchin/
+LF=$(printf '\\\012_')
+LF=${LF%_}
+make
+./mtsim -m 0 > result.csv
+
+sed -i -e "/NN/d;/aspect/d;/to/d;/^3.[1-4]/d" result.csv
+sed -i -e '1s/^/#aspect_ratio,Rad,RadS,spindle_length,time[sec],spindle_length\/(Rad*2)'"$LF"'/' result.csv
+mv result.csv ../../../Result/Simulation/Su_MTFixed.csv
+rm result.csv-e
+
+./mtsim -m 1 < enter.txt > result.csv
+
+sed -i -e "/NN/d;/aspect/d;/to/d;/^3.[1-4]/d" result.csv
+sed -i -e '1s/^/#aspect_ratio,Rad,RadS,spindle_length,time[sec],spindle_length\/(Rad*2)'"$LF"'/' < enter.txt > result.csv
+mv result.csv ../../../Result/Simulation/Su_MTVariable.csv
+rm result.csv-e
+
+make clean
+cd ../../../
